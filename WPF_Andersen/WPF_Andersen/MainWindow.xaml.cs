bbf8d3cd96c00581c3ffd61bd85206555f5763bd
Fokiruna <alekseyyarchuk@gmail.com>
2017-10-20 17:01:37 +0300

    Add style, but never used them.

--- WPF_Andersen/WPF_Andersen/MainWindow.xaml.cs
+++ WPF_Andersen/WPF_Andersen/MainWindow.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
 
 namespace WPF_Andersen
