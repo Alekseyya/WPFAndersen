395e907c7855846e2122b1f0aeb5255e9eb86417
Fokiruna <alekseyyarchuk@gmail.com>
2017-10-23 11:56:07 +0300

    Correct property command

--- WPF_Andersen/WPF_Andersen/ClientViewModel.cs
+++ WPF_Andersen/WPF_Andersen/ClientViewModel.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using DAL.Repositories;
 using Model.Entities;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using DAL.Repositories.Base;
-using WPF_Andersen.IoC;
 
 namespace WPF_Andersen
 {
@@ -21,6 +16,9 @@ namespace WPF_Andersen
         private IClientRepository _clientRepository;
         private RelayCommand _addMember;
         private RelayCommand _deleteMember;
+
+        private CancellationTokenSource tokenSource;
+        private CancellationToken token;
 
         private string _firstName;
         private string _lastName;
@@ -77,40 +75,41 @@ namespace WPF_Andersen
             }
         }
 
-        public RelayCommand DeleteMember
+        public ICommand DeleteMember
         {
             get
             {
-                return _deleteMember ??
-                       (_deleteMember = new RelayCommand(obj =>
-                           {
-                               Client client = obj as Client;
-                               if (client != null)
-                               {
-                                   _clientRepository.Delete(client.Id);
-                                   Clients.Remove(client);
-
-                               }
-                           }));
+                if (_deleteMember == null)
+                {
+                    Delete();
+                }
+                return _deleteMember;
             }
         }
-
-        public ICommand IP { get; set; }
-
-
-        public RelayCommand AddMember
+        public ICommand AddMemberTest {
+            get {
+                if (_addMember == null)
+                {
+                    Add();
+                }
+                return _addMember;
+            }
+        }
+       
+        public ClientViewModel()
         {
-            get
-            {
-                return _addMember ??
-                       (_addMember = new RelayCommand(obj =>
-                       {
-                           AddMemberOnDatabase();
-                       }));
-            }
+            ResetSourceAndToken();
+            _clientTest = new Client();
+            // получается мы тут сохдаем объект
+            _clientRepository = IoC.IoC.Get<IClientRepository>();
         }
 
-        public void AddMemberOnDatabase()
+        private void ResetSourceAndToken()
+        {
+            tokenSource = new CancellationTokenSource();
+            token = tokenSource.Token;
+        }
+        public void Add()
         {
             var client = new Client()
             {
@@ -118,6 +117,14 @@ namespace WPF_Andersen
                 LastName = ClientTest.LastName,
                 Age = ClientTest.Age
             };
+            _addMember = new RelayCommand(obj =>
+            {
+                AddMemberOnDatabase(client);
+            });
+        }
+
+        public void AddMemberOnDatabase(Client client)
+        {
             if (!_clientRepository.HasClientOnDatabase(client))
             {
                 _clientRepository.Create(client); // например после этого надо делать диспоуз?
@@ -125,25 +132,42 @@ namespace WPF_Andersen
             }
             else
                 MessageBox.Show("Такой пользователь уже существует");
-
         }
-       
-        public ClientViewModel()
+
+        public void Delete()
         {
-            _clientTest = new Client();
-            // получается мы тут сохдаем объект
-            _clientRepository = IoC.IoC.Get<IClientRepository>();
+            _deleteMember = new RelayCommand(obj =>
+            {
+                Client client = obj as Client;
+                if (client != null)
+                {
+                    DeleteMemberOnDatabase(client);
+                }
+            });
         }
 
+        public void DeleteMemberOnDatabase(Client client)
+        {
+            _clientRepository.Delete(client.Id);
+            Clients.Remove(client);
+        }
+
+       
+        //Недоделано..
         public async Task Load()
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
             await Task.Run(() =>
             {
-                //Thread.Sleep(5000);
+                Thread.Sleep(5000);
+                if (token.IsCancellationRequested)
+                {
+                    MessageBox.Show("Операция отменена");
+                    return;
+                }
                 Clients = new ObservableCollection<Client>(_clientRepository.GetList());
-            });
+            }, token);
             sw.Stop();
             TimeSpan ts = sw.Elapsed;
             
@@ -159,5 +183,7 @@ namespace WPF_Andersen
             updateWindow.DataContext = viewModel;
             updateWindow.Show();
         }
+
+        //Добавить в метод выхода(красный крестик) token.Cancel()
     }
 }
